9b30a72f7d386fef82ae3d870fd509504b734ddb
elisvieira <fernandalissesalan@gmail.com>
2023-02-11 18:25:32 +0100

    first commit

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Pessoa {
+
+	private Integer idpessoa;
+	private String nome;
+	private String email;
+
+	public Pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Pessoa(Integer idpessoa, String nome, String email) {
+		this.idpessoa = idpessoa;
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public Integer getIdpessoa() {
+		return idpessoa;
+	}
+
+	public void setIdpessoa(Integer idpessoa) {
+		this.idpessoa = idpessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
---
+++ src/interfaces/IRepository.java
@@ -0,0 +1,18 @@
+package interfaces;
+
+//Interface generica 
+import java.util.List;
+
+public interface IRepository<T> {
+
+	void save(T entity) throws Exception;
+
+	void update(T entity) throws Exception;
+
+	void delete(T entity) throws Exception;
+
+	List<T> findAll() throws Exception;
+
+	T findById(Integer id) throws Exception;
+
+}
